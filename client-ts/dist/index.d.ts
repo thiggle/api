2e560c18c867e084da5c71c231ed621b2efbc451
Matt Rickard <git@matt-rickard.com>
2023-08-09 19:47:49 -0700

    update example route

--- client-ts/dist/index.d.ts
+++ client-ts/dist/index.d.ts
@@ -7,5 +7,19 @@ type CategorizeRequest = {
 type CategorizeResponse = {
     choices: string[];
 };
+type RegexCompletionRequest = {
+    prompt: string;
+    patterns: string[];
+    max_new_tokens?: number;
+    stop_after_match?: boolean;
+    temperature?: number;
+    top_p?: number;
+    top_k?: number;
+    repetition_penalty?: number;
+};
+type RegexCompletionResponse = {
+    completion: string;
+    tokens_generated: number;
+};
 
-export { CategorizeRequest, CategorizeResponse };
+export { CategorizeRequest, CategorizeResponse, RegexCompletionRequest, RegexCompletionResponse };
